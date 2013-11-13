af68d483eebba8722bb2423ce607fdd97397abb0
maccana <mikeymccann@gmail.com>
2013-11-13 22:40:13 +0000

    Added local storage service for perstisting data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,18 @@
 'use strict';
 
 angular.module('mytodoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [
+  .controller('MainCtrl', function ($scope, localStorageService) {
+
+    /*$scope.todos = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
-    ];
-
+    ]; */
+    var todosInStore = localStorageService.get('todos');
+    $scope.todos = todosInStore && todosInStore.split('\n') || [];
+    $scope.$watch(function(){
+    	localStorageService.add('todos', $scope.todos.join('\n'));
+    });
     $scope.addTodo = function () {
     	$scope.todos.push($scope.todo);
     	$scope.todo = '';
