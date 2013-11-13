c00444920d097e878c1bc38689bea31f013b610b
maccana <mikeymccann@gmail.com>
2013-11-13 23:35:55 +0000

    Reinstalled Karma and phantomjs to resovle unit testing issues. Still problem with install. Karma server launches but aborts soon after

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,16 +3,18 @@
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope, localStorageService) {
 
-    /*$scope.todos = [
+    $scope.todos = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
-    ]; */
+    ]; 
+    /*
     var todosInStore = localStorageService.get('todos');
     $scope.todos = todosInStore && todosInStore.split('\n') || [];
     $scope.$watch(function(){
     	localStorageService.add('todos', $scope.todos.join('\n'));
     });
+*/
     $scope.addTodo = function () {
     	$scope.todos.push($scope.todo);
     	$scope.todo = '';
