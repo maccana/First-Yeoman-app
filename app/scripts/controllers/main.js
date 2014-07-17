9998f0b57270a02df7c8e0bff96b87ca961b1536
maccana <mikeymccann@gmail.com>
2014-07-16 21:41:23 -0400

    Re-push after fail

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope, localStorageService) {
+
+    $scope.todos = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ]; 
+    /*
+    var todosInStore = localStorageService.get('todos');
+    $scope.todos = todosInStore && todosInStore.split('\n') || [];
+    $scope.$watch(function(){
+    	localStorageService.add('todos', $scope.todos.join('\n'));
+    });
+*/
+    $scope.addTodo = function () {
+    	$scope.todos.push($scope.todo);
+    	$scope.todo = '';
+    }
+    $scope.removeTodo = function (index) {
+    	$scope.todos.splice(index, 1);
+    	$scope.todo = '';
+    }
+  });
