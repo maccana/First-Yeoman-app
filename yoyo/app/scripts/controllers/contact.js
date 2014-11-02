6cb8f9cc86b4013d5278d072a67a251c8ac73c8d
maccana <mikeymccann@gmail.com>
2014-11-02 15:37:17 -0500

    Added new folder for rebuild

---
+++ yoyo/app/scripts/controllers/contact.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoyoApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the yoyoApp
+ */
+angular.module('yoyoApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'Here',
+      'is',
+      'the',
+      'Contact',
+      'page',
+      'list'
+
+    ];
+    $scope.string = 'this is a string';
+
+  });
