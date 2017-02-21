f77b7d23742ee307ff0678bc64ba9c03d4b1e311
maccana <mikeymccann@gmail.com>
2017-02-21 17:57:49 -0500

    Restructed project into separate yeoman app modules and added a dedicated todo app

---
+++ yo-todo/app/scripts/controllers/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('mytodoApp')
+  // .controller('MainCtrl', function ($scope, localStorageService) {
+
+  .controller('MainCtrl', function($scope) {
+    $scope.todos = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    /*
+    var todosInStore = localStorageService.get('todos');
+    $scope.todos = todosInStore && todosInStore.split('\n') || [];
+    $scope.$watch(function(){
+    	localStorageService.add('todos', $scope.todos.join('\n'));
+    });
+*/
+    $scope.addTodo = function () {
+      console.log('ajdkasd');
+    	$scope.todos.push($scope.todo);
+    	$scope.todo = '';
+    }
+    $scope.removeTodo = function (index) {
+    	$scope.todos.splice(index, 1);
+    	$scope.todo = '';
+    }
+  });
